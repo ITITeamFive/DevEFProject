4b56014e13953232b83a90d2ea23d1304d13e5ef
shahdkhafaga <shahdkhafaga0@gmail.com>
2021-03-23 01:35:23 +0200

    Worker and Reposity start

--- AutoCare/ucWorkers.Designer.cs
+++ AutoCare/ucWorkers.Designer.cs
@@ -29,33 +29,384 @@ namespace AutoCare
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.workerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colworkerID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemGridLookUpEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit();
+            this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colworkerName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colworkerPhoneNumber = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colworkerSalary = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colworkerStartDate = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colworkerEndDate = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colWorkerAdvancePayments = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            this.panelControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.workerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.panelControl2);
+            this.panelControl1.Controls.Add(this.gridControl1);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(975, 431);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Controls.Add(this.button4);
+            this.panelControl2.Controls.Add(this.button3);
+            this.panelControl2.Controls.Add(this.button2);
+            this.panelControl2.Controls.Add(this.textBox6);
+            this.panelControl2.Controls.Add(this.textBox3);
+            this.panelControl2.Controls.Add(this.label6);
+            this.panelControl2.Controls.Add(this.label5);
+            this.panelControl2.Controls.Add(this.label2);
+            this.panelControl2.Controls.Add(this.textBox1);
+            this.panelControl2.Controls.Add(this.button1);
+            this.panelControl2.Controls.Add(this.textBox5);
+            this.panelControl2.Controls.Add(this.label4);
+            this.panelControl2.Controls.Add(this.label3);
+            this.panelControl2.Controls.Add(this.label1);
+            this.panelControl2.Controls.Add(this.textBox4);
+            this.panelControl2.Controls.Add(this.textBox2);
+            this.panelControl2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl2.Location = new System.Drawing.Point(2, 312);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(971, 117);
+            this.panelControl2.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(877, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(61, 23);
+            this.button4.TabIndex = 48;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(877, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 23);
+            this.button3.TabIndex = 47;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(877, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 23);
+            this.button2.TabIndex = 46;
+            this.button2.Text = "Add ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(637, 81);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(195, 20);
+            this.textBox6.TabIndex = 45;
+            this.textBox6.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(637, 45);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 20);
+            this.textBox3.TabIndex = 44;
+            this.textBox3.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(544, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 43;
+            this.label6.Text = "Start Date";
+            this.label6.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(544, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 42;
+            this.label5.Text = "End Date";
+            this.label5.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(544, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Salary";
+            this.label2.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(637, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 20);
+            this.textBox1.TabIndex = 40;
+            this.textBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 39;
+            this.button1.Text = "Add item";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(319, 85);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(195, 20);
+            this.textBox5.TabIndex = 38;
+            this.textBox5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Price";
+            this.label4.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(244, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Phone";
+            this.label3.Visible = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(466, 213);
+            this.label1.Location = new System.Drawing.Point(244, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 41);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Shad\'s";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 35;
+            this.label1.Text = " Name";
+            this.label1.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(319, 9);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 20);
+            this.textBox4.TabIndex = 34;
+            this.textBox4.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(319, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 20);
+            this.textBox2.TabIndex = 33;
+            this.textBox2.Visible = false;
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridControl1.DataSource = this.workerBindingSource;
+            this.gridControl1.Location = new System.Drawing.Point(2, 2);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemGridLookUpEdit1});
+            this.gridControl1.Size = new System.Drawing.Size(971, 303);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // workerBindingSource
+            // 
+            this.workerBindingSource.DataSource = typeof(AutoCare.models.Worker);
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colworkerID,
+            this.colworkerName,
+            this.colworkerPhoneNumber,
+            this.colworkerSalary,
+            this.colworkerStartDate,
+            this.colworkerEndDate,
+            this.colWorkerAdvancePayments});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // colworkerID
+            // 
+            this.colworkerID.FieldName = "workerID";
+            this.colworkerID.Name = "colworkerID";
+            this.colworkerID.Visible = true;
+            this.colworkerID.VisibleIndex = 0;
+            // 
+            // repositoryItemGridLookUpEdit1
+            // 
+            this.repositoryItemGridLookUpEdit1.AutoHeight = false;
+            this.repositoryItemGridLookUpEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemGridLookUpEdit1.DisplayMember = "workerName";
+            this.repositoryItemGridLookUpEdit1.Name = "repositoryItemGridLookUpEdit1";
+            this.repositoryItemGridLookUpEdit1.PopupView = this.gridView2;
+            this.repositoryItemGridLookUpEdit1.ValueMember = "workerID";
+            // 
+            // gridView2
+            // 
+            this.gridView2.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.gridView2.Name = "gridView2";
+            this.gridView2.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView2.OptionsView.ShowGroupPanel = false;
+            // 
+            // colworkerName
+            // 
+            this.colworkerName.FieldName = "workerName";
+            this.colworkerName.Name = "colworkerName";
+            this.colworkerName.Visible = true;
+            this.colworkerName.VisibleIndex = 1;
+            // 
+            // colworkerPhoneNumber
+            // 
+            this.colworkerPhoneNumber.FieldName = "workerPhoneNumber";
+            this.colworkerPhoneNumber.Name = "colworkerPhoneNumber";
+            this.colworkerPhoneNumber.Visible = true;
+            this.colworkerPhoneNumber.VisibleIndex = 2;
+            // 
+            // colworkerSalary
+            // 
+            this.colworkerSalary.FieldName = "workerSalary";
+            this.colworkerSalary.Name = "colworkerSalary";
+            this.colworkerSalary.Visible = true;
+            this.colworkerSalary.VisibleIndex = 3;
+            // 
+            // colworkerStartDate
+            // 
+            this.colworkerStartDate.FieldName = "workerStartDate";
+            this.colworkerStartDate.Name = "colworkerStartDate";
+            this.colworkerStartDate.Visible = true;
+            this.colworkerStartDate.VisibleIndex = 4;
+            // 
+            // colworkerEndDate
+            // 
+            this.colworkerEndDate.FieldName = "workerEndDate";
+            this.colworkerEndDate.Name = "colworkerEndDate";
+            this.colworkerEndDate.Visible = true;
+            this.colworkerEndDate.VisibleIndex = 5;
+            // 
+            // colWorkerAdvancePayments
+            // 
+            this.colWorkerAdvancePayments.FieldName = "WorkerAdvancePayments";
+            this.colWorkerAdvancePayments.Name = "colWorkerAdvancePayments";
+            this.colWorkerAdvancePayments.Visible = true;
+            this.colWorkerAdvancePayments.VisibleIndex = 6;
             // 
             // ucWorkers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ucWorkers";
-            this.Size = new System.Drawing.Size(1137, 531);
+            this.Size = new System.Drawing.Size(975, 431);
+            this.Load += new System.EventHandler(this.ucWorkers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.panelControl2.ResumeLayout(false);
+            this.panelControl2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.workerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private System.Windows.Forms.BindingSource workerBindingSource;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn colworkerID;
+        private DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit repositoryItemGridLookUpEdit1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
+        private DevExpress.XtraGrid.Columns.GridColumn colworkerName;
+        private DevExpress.XtraGrid.Columns.GridColumn colworkerPhoneNumber;
+        private DevExpress.XtraGrid.Columns.GridColumn colworkerSalary;
+        private DevExpress.XtraGrid.Columns.GridColumn colworkerStartDate;
+        private DevExpress.XtraGrid.Columns.GridColumn colworkerEndDate;
+        private DevExpress.XtraGrid.Columns.GridColumn colWorkerAdvancePayments;
     }
 }
